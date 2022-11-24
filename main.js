6ff754efb5082ac2d2e58b646f1c5a035b395540
Joao <joaopedro.ovx@gmail.com>
2022-11-23 21:17:39 -0300

    add new games

--- main.js
+++ main.js
@@ -22,9 +22,20 @@ function createCards(date, day, games){
 }
 
 document.querySelector("#cards").innerHTML=
-    createCards('24/11', 'quinta', createGame('brazil', '16:00','serbia')) +
+    createCards('24/11', 'quinta', 
+    createGame('switzerland', '07:00','cameroon') +
+    createGame('uruguay', '10:00','south-korea') +
+    createGame('portugal', '13:00','ghana')+
+    createGame('brazil', '16:00','serbia')) +
+    
     createCards('28/11', 'segunda', 
-    createGame('switzerland', '13:00','brazil') +
-    createGame('portugal', '16:00','uruguay')
-    ) + 
-    createCards('02/12', 'sexta', createGame('brazil', '16:00','cameroon'))
+    createGame('cameroon', '07:00','serbia') +
+    createGame('south-korea', '10:00','ghana') +
+    createGame('brazil', '13:00','switzerland') + 
+    createGame('portugal', '16:00','uruguay')) + 
+
+    createCards('02/12', 'sexta', 
+    createGame('south-korea', '12:00','portugal') +
+    createGame('ghana', '12:00','uruguay') +
+    createGame('serbia', '16:00','switzerland') +
+    createGame('cameroon', '16:00','brazil'))
